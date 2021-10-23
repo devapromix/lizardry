43169300a24fea0d56297eaa2337cd864ff4ef55
devapromix <maxwof@ukr.net>
2021-10-23 16:47:23 +0300

    Склеп с нежитью перекочевал в другой город.

--- worlds/elvion/locations/tavern.php
+++ worlds/elvion/locations/tavern.php
@@ -2,8 +2,44 @@
 
 if ($action == 'tavern') {
 
-	$user['title'] = 'Таверна';
-	$user['description'] = 'Вы входите в таверну. Здесь шумно и людно. Трактирщик то и дело, что успевает материть каждого на чем свет стоит и напоминать, что будет с ними, если кто-то что-то умыкнет. Ясное дело, его никто не слушает. Все столы заняты и любопытных глаз уйма. Можно даже сказать, что каждая пара глаз любопытна и каждого вновь вошедшего встречают хмурым негодованием.';
+		switch ($user['char_region']) {
+			case 1:
+				$user['title'] = 'Таверна ""';
+				$user['description'] = '';
+				break;
+			case 2:
+				$user['title'] = 'Таверна ""';
+				$user['description'] = 'Вы входите в таверну. Здесь шумно и людно. Трактирщик - старый злой гном - то и дело, что успевает материть каждого на чем свет стоит и напоминать, что будет с ними, если кто-то что-то умыкнет. Ясное дело, его никто не слушает. Все столы заняты и любопытных глаз уйма. Можно даже сказать, что каждая пара глаз любопытна и каждого вновь вошедшего встречают хмурым негодованием.';
+				break;
+			case 3:
+				$user['title'] = 'Таверна ""';
+				$user['description'] = '';
+				break;
+			case 4:
+				$user['title'] = 'Таверна ""';
+				$user['description'] = '';
+				break;
+			case 5:
+				$user['title'] = 'Таверна ""';
+				$user['description'] = '';
+				break;				
+			case 6:
+				$user['title'] = 'Таверна ""';
+				$user['description'] = '';
+				break;				
+			case 7:
+				$user['title'] = 'Таверна ""';
+				$user['description'] = '';
+				break;				
+			case 8:
+				$user['title'] = 'Таверна ""';
+				$user['description'] = '';
+				break;				
+			case 9:
+				$user['title'] = 'Таверна ""';
+				$user['description'] = '';
+				break;				
+		}
 	$user['frame'] = 'tavern';	
 	$user['links'] = array();
 	$user['links'][0]['title'] = 'Вернуться в город';
@@ -19,12 +55,12 @@ if ($action == 'tavern') {
 		$user['char_mana_cur'] = $user['char_mana_max'];
 		$user['char_gold'] -= 15;
 		update_user_table("char_gold=".$user['char_gold'].",char_life_cur=".$user['char_life_cur'].",char_mana_cur=".$user['char_mana_cur']);
-		$user['log'] = 'Вы хорошо отдохнули и набрались сил.';
+		$user['log'] = 'Вы хорошо выспались, совершенно здоровы и полны сил! И у вас высокий боевой дух!';
 	}
 	
 	if ($do == 'buy_food_in_tavern') {
-		if ($amount <= 0) die('{"error":"Количество сумок с провинтом должно быть больше 0!"}');
 		if ($user['char_food'] >= 7) die('{"error":"У Вас максимальный запас провианта!"}');
+		if ($amount <= 0) die('{"error":"Количество сумок с провинтом должно быть больше 0!"}');
 		if ($user['char_gold'] < $amount * 10) die('{"error":"Недостаточно денег!"}');
 		if ($amount + $user['char_food'] > 7) die('{"error":"Введите правильное число!"}'); 
 		$user['char_gold'] -= $amount * 10;
