c8cec18af0e2024c810e6eca79380c1a788c9b82
devapromix <maxwof@ukr.net>
2021-11-22 21:06:10 +0200

    Добавлено расовое уклонение для людей.

--- worlds/elvion/common/common.php
+++ worlds/elvion/common/common.php
@@ -410,22 +410,27 @@ function enemy_battle_round() {
 	global $user, $stat;
 	$r = '';
 	if (($user['enemy_life_cur'] > 0)&&($user['char_life_cur'] > 0)) {
-		if (rand(1, $user['char_armor'] + 10) <= rand(1, $user['enemy_armor'])) {
+		if (rand(1, $user['char_armor']) <= rand(1, $user['enemy_armor'])) {
 			if (rand(1, 100) > $user['skill_dodge']) {
 				if (rand(1, 100) > $user['skill_parry']) {
-					$d = rand($user['enemy_damage_min'], $user['enemy_damage_max']);
-					$d = get_real_damage($d, $user['char_armor'], $user['enemy_level'], $user['char_level']);
-					$stat['enemy_hits']++;
-					if ($d <= 0) {
-						$r .= $user['enemy_name'].' не может пробить вашу защиту.#';
-					} else {
-						$stat['enemy_damages'] += $d;
-						$user['char_life_cur'] -= $d;
-						if ($user['char_life_cur'] > 0) {
-							$r .= $user['enemy_name'].' ранит вас на '.$d.' HP.#';
-						}else{
-							$r .= $user['enemy_name'].' наносит удар на '.$d.' HP и убивает вас.#';
+					if (rand(1, 100) > 5) {
+						$d = rand($user['enemy_damage_min'], $user['enemy_damage_max']);
+						$d = get_real_damage($d, $user['char_armor'], $user['enemy_level'], 	$user['char_level']);
+						$stat['enemy_hits']++;
+						if ($d <= 0) {
+							$r .= $user['enemy_name'].' не может пробить вашу защиту.#';
+						} else {
+							$stat['enemy_damages'] += $d;
+							$user['char_life_cur'] -= $d;
+							if ($user['char_life_cur'] > 0) {
+								$r .= $user['enemy_name'].' ранит вас на '.$d.' HP.#';
+							} else {
+								$r .= $user['enemy_name'].' наносит удар на '.$d.' HP и убивает вас.#';
+							}
 						}
+					} else {
+						$r .= 'Ваш расовый навык позволяет уклониться от атаки '.$user['enemy_name'].'.#';
+					$stat['char_dodges']++;
 					}
 				} else {
 					$r .= 'Вы парируете атаку '.$user['enemy_name'].'.#';
