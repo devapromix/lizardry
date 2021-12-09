f0d270ff8473c3d2799378f573bb8e2b356f078c
devapromix <maxwof@ukr.net>
2021-12-09 23:24:47 +0200

    Добавлены Гильдия Кожевников и Гильдия Выживания.

--- worlds/elvion/locations/guilds.php
+++ worlds/elvion/locations/guilds.php
@@ -13,6 +13,8 @@ if ($action == 'guilds') {
 		addlink('Гильдия Воинов', 'index.php?action=guild_warrior', 2);
 		addlink('Гильдия Охотников', 'index.php?action=guild_hunter', 3);
 		addlink('Гильдия Кузнецов', 'index.php?action=guild_forge', 4);
+		addlink('Гильдия Кожевников', 'index.php?action=guild_lw', 5);
+		addlink('Гильдия Выживания', 'index.php?action=guild_surv', 6);
 	} else go_to_the_graveyard();
 	
 	$res = json_encode($user, JSON_UNESCAPED_UNICODE);	
@@ -65,7 +67,7 @@ if ($action == 'guild_warrior') {
 		$t .= 'Выбирайте навык и приступайте к тренировкам.';
 	else
 		$t .= 'К сожалению я не могу сейчас тренировать вас - нужны очки развития навыков.';
-	$t .= '##Ваши навыки:#============#';
+	$t .= '##Ваши навыки Гильдии Воинов:#============#';
 	if ($user['skill_dodge'] > 0)
 		$t .= 'Уклонение: '.$user['skill_dodge'].'/50#';
 	if ($user['skill_parry'] > 0)
@@ -152,8 +154,8 @@ if ($action == 'guild_warrior') {
 if ($action == 'guild_hunter') {
 
 	$user['title'] = 'Гильдия Охотников';
-	$t = 'Вы входите в светлый просторный зал. Вас встречает высокий эльф в добротной кожаной броне:#-Здраствуй, '.$user['char_name'].'! Добро пожаловать в Гильдию Охотников. У нас можно улучшить свои навыки или изучить новые.#Также я щедро плачу золотом за ценные охотничьи трофеи.##';
-	$t .= trophy_list();
+	$t = 'Вы входите в светлый просторный зал, обставленый чучелами различных птиц и зверей. Вас встречает высокий эльф в добротной кожаной броне и длинным луком за плечами:#-Здраствуй, '.$user['char_name'].'! Добро пожаловать в Гильдию Охотников. У нас можно улучшить свои навыки охоты или изучить новые.#Также я щедро плачу золотом за ценные охотничьи трофеи.##';
+	$t .= inv_items_list(21);
 	$user['description'] = $t;
 	$user['links'] = array();
 	addlink('Покинуть гильдию', 'index.php?action=guilds');
@@ -161,7 +163,7 @@ if ($action == 'guild_hunter') {
 
 	if ($do == 'trophy_trade') {
 		if ($user['char_life_cur'] <= 0) die('{"error":"Вам сначала нужно вернуться к жизни!"}');
-		$gold = trophy_trade();
+		$gold = inv_items_trade(21);
 		$user['description'] = 'Вы продали все свои трофеи и заработали '.$gold.' золотых монет.';
 		$user['links'] = array();
 		addlink('Назад', 'index.php?action=guild_hunter');
@@ -175,7 +177,7 @@ if ($action == 'guild_forge') {
 
 	$user['title'] = 'Гильдия Кузнецов';
 	$t = 'Вы входите в Кузницу. К вам выходит старый гном в испачканой угльной пылью одежде:#-Приветствую, '.$user['char_name'].'! Рад видеть тебя в Гильдии Кузнецов. У нас ты можешь потренироваться, отремонтировать свою экипировку, купить нужные вещи кузнеца или продать старое оружие.##';
-	$t .= swords_list();
+	$t .= inv_items_list(1);
 	$user['description'] = $t;
 	$user['links'] = array();
 	addlink('Покинуть гильдию', 'index.php?action=guilds');
@@ -183,10 +185,102 @@ if ($action == 'guild_forge') {
 
 	if ($do == 'weapon_trade') {
 		if ($user['char_life_cur'] <= 0) die('{"error":"Вам сначала нужно вернуться к жизни!"}');
-		$gold = swords_trade();
+		$gold = inv_items_trade(1);
 		$user['description'] = 'Вы продали старое оружие и заработали '.$gold.' золотых монет.';
 		$user['links'] = array();
 		addlink('Назад', 'index.php?action=guild_forge');
+	}
+
+	$res = json_encode($user, JSON_UNESCAPED_UNICODE);	
+
+}
+
+if ($action == 'guild_lw') {
+
+	$user['title'] = 'Гильдия Кожевников';
+	$t = 'В Гильдии Кожевников к вам подходит красивая эльфийка в дорогой кожаной эльфийской броне:#-Здраствуйте! Рада видеть вас в нашей гильдии. Мы рады помочь каждому путнику, кому нужна наша помощь. Я с удовольствием обучаю исскуству снятия кожи с убитых животных и достаточно дорого выкупаю уже не нужную кожаную броню.##';
+	$t .= inv_items_list(0);
+	$user['description'] = $t;
+	$user['links'] = array();
+	addlink('Покинуть гильдию', 'index.php?action=guilds');
+	addlink('Продать Броню', 'index.php?action=guild_lw&do=armor_trade', 1);
+
+	if ($do == 'armor_trade') {
+		if ($user['char_life_cur'] <= 0) die('{"error":"Вам сначала нужно вернуться к жизни!"}');
+		$gold = inv_items_trade(0);
+		$user['description'] = 'Вы продали старую кожаную броню и заработали '.$gold.' золотых монет.';
+		$user['links'] = array();
+		addlink('Назад', 'index.php?action=guild_lw');
+	}
+
+	$res = json_encode($user, JSON_UNESCAPED_UNICODE);	
+
+}
+
+if ($action == 'guild_surv') {
+
+	$user['title'] = 'Гильдия Выживания';
+	$t = ' У вас-с сейчас-с-с '.$user['char_lp'].' свободных оч. ';
+	if ($user['char_lp'] > 0)
+		$t .= 'Выбирайте навык и прис-с-ступайте к тренировкам-с-с.';
+	else
+		$t .= 'К с-сожалению я не могу с-сейчас-с-с тренировать вас-с - нужны очки развития навыков-с-с.';
+	$t .= '##Ваши навыки Гильдии Выживания:#============#';
+	if ($user['skill_run'] > 0)
+		$t .= 'Обман: '.$user['skill_run'].'/10#';
+	if ($user['skill_gold'] > 0)
+		$t .= 'Жадность: '.$user['skill_gold'].'/10#';
+	$user['description'] = 'Вы спускаетесь по ступенькам в подвальное помещение. Здесь достаточно темно и только часть помещения освещена факелами и свечами. К вам подходит худой высокий ящер в темных одеяниях:#-Здрас-с-ствуйте-с-с! Могу я вам-с-с помочь-с-с?'.$t;
+	$user['links'] = array();
+	addlink('Покинуть гильдию', 'index.php?action=guilds');
+	addlink('Информация по навыкам', 'index.php?action=guild_surv&do=info', 1);
+	addlink('Тренировать "Обман"', 'index.php?action=guild_surv&do=train_run', 2);
+	addlink('Тренировать "Жадность"', 'index.php?action=guild_surv&do=train_gold', 3);
+
+	if ($do == 'info') {
+		$user['description'] = 'Мастер с удовольствием рассказывает о навыках Гильдии Выживания, которые вам доступны для изучения..';
+		$user['links'] = array();
+		addlink('Мне уже все понятно!', 'index.php?action=guild_surv');
+		addlink('Навык "Обман"', 'index.php?action=guild_surv&do=train_run_info', 1);
+		addlink('Навык "Жадность"', 'index.php?action=guild_surv&do=train_gold_info', 2);
+	}
+
+	if ($do == 'train_run') {
+		if ($user['char_life_cur'] <= 0) die('{"error":"Вам сначала нужно вернуться к жизни!"}');
+		if ($user['char_lp'] == 0) die('{"error":"Для тренировки нужны очки развития навыков!"}');
+		if ($user['skill_run'] >= 10) die('{"error":"Вы достигли максимума в развитии навыка!"}');
+		$user['char_lp']--;
+		$user['skill_run']++;
+		$user['description'] = 'Вы тренируетесь совершать обманные движения в поединке, чтобы повысить свои шансы спастись и выйти из боя живым. В умение "Обман" вложено '.$user['skill_run'].' оч.';
+		update_user_table("char_lp=".$user['char_lp'].",skill_run=".$user['skill_run']);
+		$user['log'] = 'Вы потренировались и стали лучше!';
+		$user['links'] = array();
+		addlink('Назад', 'index.php?action=guild_surv');
+	}
+
+	if ($do == 'train_run_info') {
+		$user['description'] = 'Совершив в бою обманный маневр и перехитрив противника, вы увеличите свои шансы покинуть поле битвы живым. Вероятность обмануть противника в бою увеличена на '.strval($user['skill_run'] * 5).'%.';
+		$user['links'] = array();
+		addlink('Мне уже все понятно!', 'index.php?action=guild_surv&do=info');
+	}
+	
+	if ($do == 'train_gold') {
+		if ($user['char_life_cur'] <= 0) die('{"error":"Вам сначала нужно вернуться к жизни!"}');
+		if ($user['char_lp'] == 0) die('{"error":"Для тренировки нужны очки развития навыков!"}');
+		if ($user['skill_gold'] >= 10) die('{"error":"Вы достигли максимума в развитии навыка!"}');
+		$user['char_lp']--;
+		$user['skill_gold']++;
+		$user['description'] = 'Вы тренируетесь получше обыскивать трупы врагов, чтобы добыть побольше золота. В умение "Жадность" вложено '.$user['skill_gold'].' оч.';
+		update_user_table("char_lp=".$user['char_lp'].",skill_gold=".$user['skill_gold']);
+		$user['log'] = 'Вы потренировались и стали лучше!';
+		$user['links'] = array();
+		addlink('Назад', 'index.php?action=guild_surv');
+	}
+
+	if ($do == 'train_gold_info') {
+		$user['description'] = 'Навык позволяет вам получше обыскивать поверженых врагов и получать больше золота от монстров.';
+		$user['links'] = array();
+		addlink('Мне уже все понятно!', 'index.php?action=guild_surv&do=info');
 	}
 
 	$res = json_encode($user, JSON_UNESCAPED_UNICODE);	
