674fde718d23447b393b29cd0f8546f8f355e214
devapromix <maxwof@ukr.net>
2022-08-21 09:26:26 +0300

    Доб. свитки как предметы. Свиток портала города.

--- worlds/elvion/common/common.php
+++ worlds/elvion/common/common.php
@@ -105,6 +105,7 @@ function equip_item($item_ident) {
 		case 11:
 		case 12:
 		case 13:
+		case 25:
 			$user['char_gold'] = $user['char_gold'] - $item['item_price'];
 			save_to_log($item['item_name'].' - предмет куплен и перемещен в инвентарь.');
 			add_item($item['item_ident']);
@@ -168,6 +169,7 @@ function pickup_equip_item() {
 		case 12:
 		case 13:
 		case 21:
+		case 25:
 			$r = 'Вы забираете '.$item['item_name'].' себе.';
 			save_to_log($item['item_name'].' - предмет перемещен в инвентарь.');
 			add_item($item['item_ident']);
@@ -206,6 +208,9 @@ function item_values($item_ident) {
 			break;
 		case 13:
 			return $item['item_name'].','.strval($item['item_level']*25).','.get_region_item_level($item['item_level']).','.$item['item_price'];
+			break;
+		case 25:
+			return $item['item_name'].','.strval($item['item_level']).','.get_region_item_level($item['item_level']).','.$item['item_price'];
 			break;
 	}
 }
@@ -408,7 +413,7 @@ function gen_loot() {
 
 		$next = true;
 		$loot_level = $user['char_region'];
-		$loot_type_array = [0,1,8,9,10,11];
+		$loot_type_array = [0,1,8,9,10,11,25];
 		$loot_type = $loot_type_array[array_rand($loot_type_array)];
 		
 		if (($loot_level > 1)&&(rand(0, 4) == 0))
@@ -813,6 +818,9 @@ function item_info($item_ident) {
 		case 13:
 			$ef = 'Покрывает оружие ядом на '.strval($item['item_level']*5).' битв.';
 			break;
+		case 25:
+			$ef = 'Открывает портал в город.';
+			break;
 	}
 	if ($ef == '')
 		die('{"item":""}');
@@ -873,6 +881,14 @@ function use_item($item_ident) {
 				$user['char_mana_cur'] = $user['char_mana_max'];
 			update_user_table("char_life_cur=".$user['char_life_cur'].",char_mana_cur=".$user['char_mana_cur']);
 			$result = ',"char_life_cur":"'.$user['char_life_cur'].'","char_life_max":"'.$user['char_life_max'].'","char_mana_cur":"'.$user['char_mana_cur'].'","char_mana_max":"'.$user['char_mana_max'].'"';
+			break;
+		case 25:
+			if ($user['char_mana_cur'] >= 8) {
+				item_modify($item_ident, -1);
+				$user['char_mana_cur'] -= 8;
+				update_user_table("char_mana_cur=".$user['char_mana_cur']);
+				$result = ',"char_mana_cur":"'.$user['char_mana_cur'].'","char_mana_max":"'.$user['char_mana_max'].'"';
+			} else die('{"error":"Нужно больше маны!"}');
 			break;
 	}
 	return $result;
