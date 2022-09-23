17ed7646f49c47a24e891477710ec9c028cb8e33
devapromix <maxwof@ukr.net>
2022-09-23 21:50:54 +0300

    Поправлен диалог в лагере.

--- worlds/elvion/locations/campfire.php
+++ worlds/elvion/locations/campfire.php
@@ -3,7 +3,7 @@
 if ($action == 'campfire') {
 	
 	if ($user['char_life_cur'] <= 0) die('{"error":"Вам сначала нужно вернуться к жизни!"}');
-	if ($user['char_food'] <= 0) die('{"error":"Вы не можете здесь отдыхать - нет провизии!"}');
+	if ($user['char_food'] <= 0) die('{"info":"Вы не можете здесь отдыхать - нет провизии!"}');
 	if ($user['char_life_cur'] == $user['char_life_max']) die('{"info":"Вам незачем отдыхать - вы здоровы и полны сил!"}');
 
 	$user['title'] = 'Лагерь!';
