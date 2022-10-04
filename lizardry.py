456211cc938ef0e2c19e9f7dc481542348dabb06
devapromix <maxwof@ukr.net>
2022-10-04 15:37:57 +0300

    Переход по локациям в тестовом клиенте на пайтон.

--- lizardry.py
+++ lizardry.py
@@ -17,10 +17,11 @@ def enter():
     flag = response.text == '{"login":"ok"}'
     if flag:
         print("Добро пожаловать в Лизардри в." + VERSION)
+        print("----------")
     return login, flag
 
-def town(login):
-    response = requests.get('http://lizardry.pp.ua/' + SERVER + '/index.php?username=' + login.username + '&userpass=' + login.userpass + '&action=town')
+def location(login, url):
+    response = requests.get('http://lizardry.pp.ua/' + SERVER + '/' + url + '&username=' + login.username + '&userpass=' + login.userpass)
     jsons = json.loads(response.text)
     #print(response.text)
     #print(jsons['user_name'])
@@ -34,11 +35,17 @@ def town(login):
     print(jsons['description'])
     n = 1
     for j in jsons['links']:
-        print(str(n) + '. ' + j['title'])#+'->'+j['link'])
+        print(str(n) + '. ' + j['title'])
         n = n + 1
-    choice = input("> ")
+    choice = input(">")
+    n = 1
+    for j in jsons['links']:
+        if n == int(choice):
+            location(login, j['link'])
+            break
+        n = n + 1
 
 if __name__ == "__main__":
     login, flag = enter()
     if flag:
-        town(login)
+        location(login, "index.php?action=town")
