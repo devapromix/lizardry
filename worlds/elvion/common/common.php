906df8fcc2a634d0be03302e83cf63cb11754764
devapromix <maxwof@ukr.net>
2022-11-03 15:41:43 +0200

    Revert "Рефакторинг - класс Event."
    This reverts commit a465ba50f9b3071cab316bfbe0a878cc7e458230.

--- worlds/elvion/common/common.php
+++ worlds/elvion/common/common.php
@@ -108,6 +108,23 @@ function post_param($value, $default) {
 	return $res;
 }
 
+function add_event($type, $name, $level = 1, $gender = 0, $str = '', $loc_name = '') {
+	global $connection, $user, $tb_events;
+	$query = "INSERT INTO ".$tb_events." (event_type,event_char_gender,event_char_name,event_char_level,event_str,event_loc) VALUES(".$type.", ".$gender.", '".$name."', ".$level.", '".$str."', '".$loc_name."')";
+	if (!mysqli_query($connection, $query)) {
+		die('{"error":"Ошибка сохранения данных: '.mysqli_error($connection).'"}');
+	}
+}
+
+function get_events() {
+	global $connection, $tb_events;
+	$query = "SELECT event_type, event_char_gender, event_char_name, event_char_level ,event_str, event_loc FROM ".$tb_events." ORDER BY id  DESC LIMIT 0, 15";
+	$result = mysqli_query($connection, $query) 
+		or die('{"error":"Ошибка считывания данных: '.mysqli_error($connection).'"}');
+	$events = $result->fetch_all(MYSQLI_ASSOC);
+	return json_encode($events, JSON_UNESCAPED_UNICODE);
+}
+
 function get_messages() {
 	global $tb_chat, $connection;
 
