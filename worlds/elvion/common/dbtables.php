b46a8926ad00d036bd1d86bd7bdb9f3e5d4342bb
devapromix <maxwof@ukr.net>
2022-11-04 12:41:39 +0200

    Уд. не нужные таблицы.

--- worlds/elvion/common/dbtables.php
+++ worlds/elvion/common/dbtables.php
@@ -2,12 +2,10 @@
 
 $tb_user      = 'lizardry_users';
 $tb_item      = 'lizardry_items';
-$tb_chat      = 'lizardry_messages';
 $tb_enemy     = 'lizardry_enemies';
 $tb_regions   = 'lizardry_regions';
 $tb_locations = 'lizardry_locations';
 $tb_events    = 'lizardry_events';
-$tb_log       = 'lizardry_logs';
 $tb_phrases   = 'lizardry_phrases';
 
 ?>
