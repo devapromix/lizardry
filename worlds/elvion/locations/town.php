3425e099b31c121be8631d2116929fc90f598882
devapromix <maxwof@ukr.net>
2022-11-15 23:55:02 +0200

    Исправлена ошибка с отображением имени города.

--- worlds/elvion/locations/town.php
+++ worlds/elvion/locations/town.php
@@ -1,13 +1,13 @@
 <?php
 
 if ($action == 'town') {
-	$user['title'] = $user['char_region_town_name'];
+	$user['title'] = $user['class']['location']->get_town_name();
 	if ($user['char_life_cur'] > 0)
 		$user['description'] = $user['class']['location']->get_town_description();
 	else $user['class']['location']->shades();
 	$user['links'] = array();
 	if ($user['char_life_cur'] > 0) {	
-		$user['class']['location']->go_to_the_gate('Покинуть '.$user['char_region_town_name']);
+		$user['class']['location']->go_to_the_gate('Покинуть '.$user['title']);
 		Location::addlink('Посетить Таверну', 'index.php?action=tavern', 1);
 		Location::addlink('Отправиться в Банк', 'index.php?action=bank', 2);
 		Location::addlink('Идти в Магическую Башню', 'index.php?action=magictower', 3);
