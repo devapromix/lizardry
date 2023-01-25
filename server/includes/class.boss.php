4de4cffc61c360147118486cb2a4f2638c0ad61e
devapromix <maxwof@ukr.net>
2023-01-25 07:26:40 +0200

    Исправлена ошибка с неправильной реестрацией убитых боссов.

--- server/includes/class.boss.php
+++ server/includes/class.boss.php
@@ -4,21 +4,21 @@
 		
 		public const START_ID = 800;
 		
-		public static function kill($region_ident) {
+		public static function kill(int $region_ident) {
 			global $user;
 			$user['stat_boss_kills']++;
 			$bosses = json_decode($user['char_bosses'], true);
 			$n = count($bosses);
-			$bosses[$n]['id'] = $region_ident;
+			$bosses[$n]['id'] = strval($region_ident);
 			$bosses[$n]['killed'] = 1;
 			$user['char_bosses'] = json_encode($bosses, JSON_UNESCAPED_UNICODE);
 			User::update("char_bosses='".$user['char_bosses']."'");
 		}
 
-		public static function is_killed($region_ident) {
+		public static function is_killed(int $region_ident) {
 			global $user;
 			$bosses = $user['char_bosses'];
-			$pos = strripos($bosses, '"id":"'.$region_ident.'"');
+			$pos = strripos($bosses, '"id":"'.strval($region_ident).'"');
 			if ($pos === false) {
 				return false;
 			} else {
