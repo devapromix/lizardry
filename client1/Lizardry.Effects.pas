4f3fbe4cbdf8ed91136ffb4a83f206de522854b6
devapromix <maxwof@ukr.net>
2023-01-30 09:22:11 +0200

    Рефакторинг (эффекты).

---
+++ client1/Lizardry.Effects.pas
@@ -0,0 +1,11 @@
+﻿unit Lizardry.Effects;
+
+interface
+
+const
+  EffectStr: array [0 .. 4] of string = ('Нет', 'Благословение', 'Регенерация',
+    'Кража Жизни', 'Отражение Урона');
+
+implementation
+
+end.
