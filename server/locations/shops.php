9c0d3e126f76bc9d9f5d9bb62240444efce18d87
devapromix <maxwof@ukr.net>
2023-02-20 22:24:08 +0200

    В магазине воров можно прикупить отмычки.

--- server/locations/shops.php
+++ server/locations/shops.php
@@ -128,7 +128,7 @@ if ($action == 'black_market') {
 	$user['links'] = array();
 	Location::addlink('Покинуть рынок', 'index.php?action=denofthieves');
 
-	$user['class']['item']->add_item_to_shop(1, 701);
+	$user['class']['item']->add_item_to_shop(1, 951);
 
 
 	if (($do == 'buy') && ($itemslot >= '1') && ($itemslot <= '6'))
