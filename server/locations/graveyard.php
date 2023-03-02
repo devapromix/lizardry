373b0d7d210b5a1498191644361b927bb430da3d
devapromix <maxwof@ukr.net>
2023-03-02 14:44:12 +0200

    Сообщ. в лог при воскрешении.

--- server/locations/graveyard.php
+++ server/locations/graveyard.php
@@ -19,6 +19,7 @@ if ($action == 'graveyard') {
 		$user['char_mana_cur'] = 0;
 		User::update("char_life_cur=".$user['char_life_cur'].",char_mana_cur=".$user['char_mana_cur']);
 		$user['description'] = 'Вы открываете глаза и понимаете, что вернулись в мир живых. На тело наваливаетеся сильная усталость, кружится голова. Все происходящее кажется сном. Нужно отдохнуть и набраться сил.';
+		$user['log'] = 'Вы вернулись к жизни!';
 		$user['links'] = array();
 		$user['class']['location']->get_graveyard_links();
 	}
